7836426527085d29da0c56ae3f34338f7e948453
Tejas M R <tejasmr@outlook.com>
2022-09-22 15:28:32 +0530

    Added query by id with graph ql

--- WebApplicationToo/WebApplicationToo/Data/GraphQL/CollegeQuery.cs
+++ WebApplicationToo/WebApplicationToo/Data/GraphQL/CollegeQuery.cs
@@ -1,3 +1,4 @@
+using GraphQL;
 using GraphQL.Types;
 using WebApplicationToo.Data.GraphQL.Types;
 using WebApplicationToo.Repositories;
@@ -8,6 +9,11 @@ namespace WebApplicationToo.Data.GraphQL {
         public CollegeQuery(StudentRepository repository)
         {
             Field<ListGraphType<StudentType>>("students", resolve: context => repository.GetStudents());
+            Field<StudentType>("studentsById", arguments: new QueryArguments(new QueryArgument<NonNullGraphType<IntGraphType>> { Name = "id" }), resolve: context =>
+                {
+                    var id = context.GetArgument<int>("id");
+                    return repository.GetStudentWith(id);
+                });
         }
     }
 }
